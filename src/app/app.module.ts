d64c81896b660f2ea205ae1351a6c5b0f2433a67
meehawo <myf.goralczyk@gmail.com>
2023-09-25 20:38:30 +0200

    galeria gotowa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CompAComponent } from './comp-a/comp-a.component';
+import { CompBComponent } from './comp-b/comp-b.component';
+import { HomeComponent } from './home/home.component';
+import { PhotoComponent } from './photo/photo.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CompAComponent,
+    CompBComponent,
+    HomeComponent,
+    PhotoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
